b1029e8fe6ac36c042dbc443e14ab9cd3443efbd
Jong <jm991014@gmail.com>
2024-04-15 01:39:06 +0900

    [Add] #2 - add Dimens
    for padding, size, etc

---
+++ app/src/main/java/com/example/pillinTimeAndroid/presentation/Dimens.kt
@@ -0,0 +1,17 @@
+package com.example.pillinTimeAndroid.presentation
+
+import androidx.compose.ui.unit.dp
+
+object Dimens {
+    // height for basic button, editText, etc
+    val BasicSize = 64.dp
+
+    // padding for basic button, editText, etc
+    val BasicPadding = 26.dp
+    val BasicTopPadding = 43.dp
+
+    val BottomNavSize = 56.dp
+    val NavIconSize = 28.dp
+
+    val TextFieldHorPadding = 22.dp
+}
