343d327e29b9024c1c474ebaf989e561732eb605
Jong <jm991014@gmail.com>
2024-05-06 20:35:09 +0900

    [Add] #2 - Application 파일 생성

---
+++ app/src/main/java/com/example/pillinTimeAndroid/PillinTimeApplication.kt
@@ -0,0 +1,9 @@
+package com.example.pillinTimeAndroid
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class PillinTimeApplication: Application() {
+
+}
