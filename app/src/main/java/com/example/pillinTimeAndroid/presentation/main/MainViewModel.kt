fcdb7006e7e6f79a5cb148f7b94008fb6c51e7e0
Jong <jm991014@gmail.com>
2024-05-18 14:30:54 +0900

    [Fix] #7 - MainVM 앱 진입 로직 변경

--- app/src/main/java/com/example/pillinTimeAndroid/presentation/main/MainViewModel.kt
+++ app/src/main/java/com/example/pillinTimeAndroid/presentation/main/MainViewModel.kt
@@ -1,9 +1,11 @@
 package com.example.pillinTimeAndroid.presentation.main
 
+import android.util.Log
 import androidx.compose.runtime.State
 import androidx.compose.runtime.mutableStateOf
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import com.example.pillinTimeAndroid.domain.repository.UserRepository
 import com.example.pillinTimeAndroid.domain.usecase.session.ReadUserSession
 import com.example.pillinTimeAndroid.presentation.nvgraph.Route
 import dagger.hilt.android.lifecycle.HiltViewModel
@@ -16,8 +18,8 @@ import javax.inject.Inject
 @HiltViewModel
 class MainViewModel @Inject constructor(
     private val readUserSession: ReadUserSession,
+    private val userRepository: UserRepository
 ) : ViewModel() {
-
     private val _splashCondition = mutableStateOf(true)
     val splashCondition: State<Boolean> = _splashCondition
 
@@ -26,9 +28,18 @@ class MainViewModel @Inject constructor(
 
     init {
         viewModelScope.launch {
-            val user = readUserSession().firstOrNull()
-            if (user != null) {
-                _startDestination.value = Route.BottomNavigation.route
+            val hasToken = readUserSession().firstOrNull().orEmpty().isNotEmpty()
+            if (hasToken) {
+                val result = userRepository.initClient()
+                result.onSuccess {
+                    if (!it.result.isManager && it.result.relationList.isEmpty()) {
+                        _startDestination.value = Route.AppStartNavigationV2.route
+                    } else if (it.result.isManager || it.result.relationList.isNotEmpty()) {
+                        _startDestination.value = Route.BottomNavigation.route
+                    }
+                }.onFailure {
+                    Log.d("MainViewModel", "failed to init: ${it.cause}")
+                }
             } else {
                 _startDestination.value = Route.AppStartNavigation.route
             }
