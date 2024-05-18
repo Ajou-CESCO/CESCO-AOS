fc4f08e080e5ba944670cb1c8c6fca8272da34b0
Jong <jm991014@gmail.com>
2024-05-18 22:51:04 +0900

    [Fix] #7 - 앱 진입 분기 UseCase 수정

--- app/src/main/java/com/example/pillinTimeAndroid/domain/usecase/session/ReadUserSession.kt
+++ app/src/main/java/com/example/pillinTimeAndroid/domain/usecase/session/ReadUserSession.kt
@@ -1,14 +1,13 @@
 package com.example.pillinTimeAndroid.domain.usecase.session
 
-import com.example.pillinTimeAndroid.domain.entity.User
-import com.example.pillinTimeAndroid.domain.repository.UserRepository
+import com.example.pillinTimeAndroid.data.local.LocalUserDataSource
 import kotlinx.coroutines.flow.Flow
 import javax.inject.Inject
 
 class ReadUserSession @Inject constructor(
-    private val userRepository: UserRepository
+    private val localUserDataSource: LocalUserDataSource
 ) {
-    operator fun invoke(): Flow<User?> {
-        return userRepository.readUserSession()
+    operator fun invoke(): Flow<String?> {
+        return localUserDataSource.getAccessToken()
     }
 }
