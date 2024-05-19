355ac24b77f8e9031092e5d3e8e279f657e00826
Jong <jm991014@gmail.com>
2024-05-20 01:15:36 +0900

    [Feat] #7 - pull to refresh 각 화면에 적용

--- app/src/main/java/com/example/pillinTimeAndroid/presentation/home/HomeScreen.kt
+++ app/src/main/java/com/example/pillinTimeAndroid/presentation/home/HomeScreen.kt
@@ -1,78 +1,81 @@
 package com.example.pillinTimeAndroid.presentation.home
 
 import androidx.compose.foundation.ExperimentalFoundationApi
-import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.pager.HorizontalPager
 import androidx.compose.foundation.pager.rememberPagerState
-import androidx.compose.foundation.rememberScrollState
-import androidx.compose.foundation.verticalScroll
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableIntStateOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
-import androidx.compose.ui.text.buildAnnotatedString
-import androidx.compose.ui.text.withStyle
-import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavController
-import com.example.pillinTimeAndroid.presentation.Dimens.BasicPadding
 import com.example.pillinTimeAndroid.presentation.home.components.ClientListBar
-import com.example.pillinTimeAndroid.presentation.home.components.DoseCard
-import com.example.pillinTimeAndroid.presentation.home.components.HealthCard
-import com.example.pillinTimeAndroid.ui.theme.Gray5
-import com.example.pillinTimeAndroid.ui.theme.Gray90
-import com.example.pillinTimeAndroid.ui.theme.PillinTimeTheme
-import com.example.pillinTimeAndroid.ui.theme.Primary60
-import com.example.pillinTimeAndroid.ui.theme.White
-import com.example.pillinTimeAndroid.ui.theme.shapes
+import com.example.pillinTimeAndroid.presentation.home.components.HomeDetailPage
+import com.example.pillinTimeAndroid.presentation.main.MainViewModel
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 
 @OptIn(ExperimentalFoundationApi::class)
 @Composable
 fun HomeScreen(
     viewModel: HomeViewModel = hiltViewModel(),
+    mainViewModel: MainViewModel = hiltViewModel(),
     navController: NavController
 ) {
-    val userDetails by viewModel.userDetails.collectAsState()
+    val userDetails by mainViewModel.userDetails.collectAsState()
+    val userDoseLog by mainViewModel.userDoseLog.collectAsState()
+    val relationInfoList by mainViewModel.relationInfoList.collectAsState()
+    val relationUserNames = relationInfoList.map { it.memberName }
     var selectedUser by remember { mutableStateOf<String?>(null) }
-    val users = viewModel.userList
-    val pagerState = rememberPagerState(pageCount = { users.size })
-    var selectedUserIndex by remember { mutableStateOf(0) }
+//    val relationInfoList by mainViewModel.relationInfoList
+//    val userDetails by viewModel.userDetails.collectAsState()
+//    val userDetailsV2 = viewModel.userInfo
+    var isRefreshing by remember { mutableStateOf(false) }
+    val pagerState = rememberPagerState(pageCount = { relationUserNames.size })
+    var selectedUserIndex by remember { mutableIntStateOf(0) }
+    var isPageChanging by remember { mutableStateOf(false) }
+    val scope = rememberCoroutineScope()
 
-    Scaffold(
-        topBar = {
-            if (userDetails?.isManager == true) {
-                ClientListBar(
-                    users,
-                    selectedIndex = selectedUserIndex,
-                    onProfileSelected = { index ->
-                        selectedUserIndex = index
-                    }
-                )
-            }
-        },
-        content = { innerPadding ->
-            HorizontalPager(
-                state = pagerState,
-                modifier = Modifier
-                    .fillMaxSize()
-                    .padding(innerPadding),
-            ) { page ->
-                UserDetailScreen(userName = users[page])
-            }
+    Box(
+        modifier = Modifier.fillMaxSize()
+    ) {
+        if (userDetails?.isManager == true) {
+            ClientListBar(
+                relationUserNames,
+                selectedIndex = selectedUserIndex,
+                onProfileSelected = { index ->
+                    selectedUserIndex = index
+                }
+            )
         }
-    )
+        HomeDetailPage(
+            modifier = Modifier,
+            navController = navController,
+            userDetail = userDetails,
+            onPullRefresh = {
+                scope.launch {
+                    isRefreshing = true
+                    userDetails?.memberId?.let { mainViewModel.getUserDoseLog(it) }
+                    delay(1000)
+                    isRefreshing = false
+                }
+            },
+            isRefreshing = isRefreshing,
+            userDoseLog = userDoseLog
+        )
+    }
+    LaunchedEffect(userDetails) {
+        userDetails?.let {
+            mainViewModel.getUserDoseLog(it.memberId)
+        }
+    }
 
     LaunchedEffect(selectedUserIndex) {
         pagerState.animateScrollToPage(selectedUserIndex)
@@ -80,44 +83,12 @@ fun HomeScreen(
     LaunchedEffect(pagerState.currentPage) {
         selectedUserIndex = pagerState.currentPage
     }
+    LaunchedEffect(selectedUserIndex) {
+        if (!isPageChanging) {
+            isPageChanging = true
+            pagerState.animateScrollToPage(selectedUserIndex)
+            isPageChanging = false
+        }
+    }
 }
 
-@Composable
-fun UserDetailScreen(userName: String) {
-    val scrollState = rememberScrollState()
-    val textStyle = PillinTimeTheme.typography.logo3Medium.copy(color = Gray90)
-    val greetingText = buildAnnotatedString {
-        withStyle(style = textStyle.toSpanStyle().copy(color = Primary60)) {
-            append(userName)
-        }
-        append("님,\n오늘 하루도 화이팅이에요!")
-    }
-    Column(
-        modifier = Modifier
-            .fillMaxSize()
-            .background(Gray5)
-            .verticalScroll(scrollState)
-            .padding(horizontal = BasicPadding),
-    ) {
-        Text(
-            text = greetingText,
-            style = textStyle
-        )
-        Spacer(modifier = Modifier.height(17.dp))
-        Column(
-            modifier = Modifier
-                .clip(shapes.small)
-                .background(White)
-        ) {
-            DoseCard()
-        }
-        Spacer(modifier = Modifier.height(23.dp))
-        Column(
-            modifier = Modifier
-                .clip(shapes.small)
-                .background(White)
-        ) {
-            HealthCard()
-        }
-    }
-}
--- app/src/main/java/com/example/pillinTimeAndroid/presentation/schedule/ScheduleScreen.kt
+++ app/src/main/java/com/example/pillinTimeAndroid/presentation/schedule/ScheduleScreen.kt
@@ -1,118 +1,101 @@
 package com.example.pillinTimeAndroid.presentation.schedule
 
 import androidx.compose.foundation.ExperimentalFoundationApi
-import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.pager.HorizontalPager
 import androidx.compose.foundation.pager.rememberPagerState
-import androidx.compose.foundation.rememberScrollState
-import androidx.compose.foundation.verticalScroll
-import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableIntStateOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
+import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavController
-import com.example.pillinTimeAndroid.presentation.Dimens
-import com.example.pillinTimeAndroid.presentation.common.CustomWeekCalendar
 import com.example.pillinTimeAndroid.presentation.home.components.ClientListBar
+import com.example.pillinTimeAndroid.presentation.main.MainViewModel
 import com.example.pillinTimeAndroid.presentation.schedule.components.ScheduleAddButton
-import com.example.pillinTimeAndroid.presentation.schedule.components.ScheduleCard
-import com.example.pillinTimeAndroid.ui.theme.Gray5
+import com.example.pillinTimeAndroid.presentation.schedule.components.ScheduleDetailPage
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 
 @OptIn(ExperimentalFoundationApi::class)
 @Composable
 fun ScheduleScreen(
-    viewModel: ScheduleViewModel,
+    viewModel: ScheduleViewModel = hiltViewModel(),
+    mainViewModel: MainViewModel = hiltViewModel(),
     navController: NavController
 ) {
-    val userDetails by viewModel.userDetails.collectAsState()
+    val userDetails by mainViewModel.userDetails.collectAsState()
+    val userDoseLog by mainViewModel.userDoseLog.collectAsState()
+    val relationInfoList by mainViewModel.relationInfoList.collectAsState()
+    val relationUserNames = relationInfoList.map { it.memberName }
+    val scope = rememberCoroutineScope()
+    var isRefreshing by remember { mutableStateOf(false) }
+
     val users = viewModel.userList
-    val pagerState = rememberPagerState(pageCount = { users.size })
-    var selectedUserIndex by remember { mutableStateOf(0) }
-
-    Scaffold(
-        topBar = {
+    val pagerState = rememberPagerState(
+        pageCount = {
             if (userDetails?.isManager == true) {
-                Column() {
-                    ClientListBar(
-                        users,
-                        selectedIndex = selectedUserIndex,
-                        onProfileSelected = { index ->
-                            selectedUserIndex = index
-                        }
-                    )
-                    CustomWeekCalendar(
-                        modifier = Modifier.background(Gray5),
-                        isSelectable = false,
-                        onDaySelected = {}
-                    )
-                }
-            }
-
-        },
-        content = { innerPadding ->
-            HorizontalPager(
-                state = pagerState,
-                modifier = Modifier
-                    .fillMaxSize()
-                    .padding(innerPadding),
-            ) { page ->
-                Box (
-                    modifier = Modifier
-                ){
-                    ScheduleDetailScreen()
-                    ScheduleAddButton(
-                        Modifier
-                            .align(Alignment.BottomEnd)
-                            .padding(bottom = 12.dp, end = 16.dp),
-                    ) {
-                        navController.navigate("scheduleAddScreen")
-                    }
-                }
-
-            }
+                users.size
+            } else 1
         }
     )
-
+    var selectedUserIndex by remember { mutableIntStateOf(0) }
+    Box(
+        modifier = Modifier.fillMaxSize()
+    ) {
+        if (userDetails?.isManager == true) {
+            ClientListBar(
+                relationUserNames,
+                selectedIndex = selectedUserIndex,
+                onProfileSelected = { index ->
+                    selectedUserIndex = index
+                }
+            )
+        }
+        ScheduleDetailPage(
+            modifier = Modifier,
+            isManager = userDetails?.isManager == true,
+            onPullRefresh = {
+                scope.launch {
+                    isRefreshing = true
+                    mainViewModel.getInitData()
+                    userDetails?.memberId?.let { mainViewModel.getUserDoseLog(it) }
+                    delay(1000)
+                    isRefreshing = false
+                }
+            },
+            isRefreshing = isRefreshing,
+            userDoseLog = userDoseLog
+        )
+        ScheduleAddButton(
+            Modifier
+                .align(Alignment.BottomEnd)
+                .padding(bottom = 12.dp, end = 16.dp),
+        ) {
+            navController.navigate("scheduleAddScreen")
+        }
+    }
+    LaunchedEffect(userDetails) {
+        userDetails?.let {
+            mainViewModel.getUserDoseLog(it.memberId)
+        }
+    }
+    LaunchedEffect(true) {
+        userDetails?.memberId?.let { mainViewModel.getUserDoseLog(it) }
+    }
     LaunchedEffect(selectedUserIndex) {
         pagerState.animateScrollToPage(selectedUserIndex)
     }
     LaunchedEffect(pagerState.currentPage) {
         selectedUserIndex = pagerState.currentPage
-    }
-}
-
-@Composable
-fun ScheduleDetailScreen() {
-    val scrollState = rememberScrollState()
-
-    Box(
-        modifier = Modifier
-            .fillMaxSize()
-            .background(Gray5)
-            .verticalScroll(scrollState)
-            .padding(horizontal = Dimens.BasicPadding),
-        contentAlignment = Alignment.Center,
-
-        ) {
-        Spacer(modifier = Modifier.height(20.dp))
-        Column() {
-            ScheduleCard(state = 0)
-            ScheduleCard(state = 1)
-            ScheduleCard(state = 2)
-        }
-
     }
 }
