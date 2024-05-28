29049d304c3c5b0cddcf5e2551c684e1749cad4d
Jong <jm991014@gmail.com>
2024-05-28 14:09:44 +0900

    [Add] #10 - QR reader dependency

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -131,4 +131,10 @@ dependencies {
     implementation(libs.firebase.messaging)
     implementation(libs.firebase.analytics)
     implementation(libs.firebase.crashlytics)
+
+    // QR code
+    implementation (libs.androidx.camera.camera2)
+    implementation (libs.androidx.camera.lifecycle)
+    implementation (libs.androidx.camera.view)
+    implementation (libs.barcode.scanning)
 }
