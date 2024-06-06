8679cab91e6f8a4da17ddfc49352c69f4d3464bc
Jong <jm991014@gmail.com>
2024-06-06 10:54:06 +0900

    [Feat] #10 - 찌르기 푸시알림

--- app/src/main/java/com/example/pillinTimeAndroid/presentation/schedule/ScheduleScreen.kt
+++ app/src/main/java/com/example/pillinTimeAndroid/presentation/schedule/ScheduleScreen.kt
@@ -1,11 +1,13 @@
 package com.example.pillinTimeAndroid.presentation.schedule
 
+import android.util.Log
 import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.pager.HorizontalPager
 import androidx.compose.foundation.pager.rememberPagerState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
@@ -43,16 +45,39 @@ fun ScheduleScreen(
     val relationUserNames = relationInfoList.map { it.memberName }
     val scope = rememberCoroutineScope()
     var isRefreshing by remember { mutableStateOf(false) }
+    var selectedUserIndex by remember { mutableIntStateOf(0) }
 
-    val users = viewModel.userList
+    // when manager, memberId = current profile index memberId, else memberId = userDetails.memberId
+    val memberId = if (userDetails?.isManager == true) {
+        if (relationInfoList.isNotEmpty()) {
+            relationInfoList[selectedUserIndex].memberId
+        } else {
+            null
+        }
+    } else {
+        userDetails?.memberId
+    }
+
+    val buttonState = when (userDetails?.isManager) {
+        false -> userDetails?.cabinetId != 0
+        relationInfoList.isNotEmpty() -> relationInfoList[selectedUserIndex].cabinetId != 0
+        else -> false
+    }
+
+    val isButtonVisible: Boolean = when {
+        // manager
+        (userDetails?.isManager == true && relationInfoList.isEmpty()) -> false
+        // client
+        else -> true
+    }
+
     val pagerState = rememberPagerState(
         pageCount = {
             if (userDetails?.isManager == true) {
-                users.size
+                relationUserNames.size
             } else 1
         }
     )
-    var selectedUserIndex by remember { mutableIntStateOf(0) }
     Box(
         modifier = Modifier.fillMaxSize()
     ) {
@@ -66,42 +91,61 @@ fun ScheduleScreen(
                     selectedIndex = selectedUserIndex,
                     onProfileSelected = { index ->
                         selectedUserIndex = index
+                        scope.launch {
+                            pagerState.scrollToPage(selectedUserIndex)
+                        }
                     }
                 )
             }
-            if(userDetails?.isManager == true && relationInfoList.isEmpty()) {
+
+            if (userDetails?.isManager == true && relationInfoList.isEmpty()) {
                 ManagerEmptyView(navController)
             } else {
-                ScheduleDetailPage(
-                    modifier = Modifier.zIndex(-1f),
-                    isManager = userDetails?.isManager == true,
-                    onPullRefresh = {
-                        scope.launch {
-                            isRefreshing = true
-                            mainViewModel.getInitData()
-                            userDetails?.memberId?.let { mainViewModel.getUserDoseLog(it) }
-                            delay(1000)
-                            isRefreshing = false
+                HorizontalPager(
+                    state = pagerState,
+                    modifier = Modifier.fillMaxWidth()
+                ) {
+                    ScheduleDetailPage(
+                        modifier = Modifier.zIndex(-1f),
+                        isManager = userDetails?.isManager == true,
+                        onPullRefresh = {
+                            scope.launch {
+                                isRefreshing = true
+                                mainViewModel.getInitData()
+                                if (memberId != null) {
+                                    mainViewModel.getUserDoseLog(memberId)
+                                }
+                                delay(1000)
+                                isRefreshing = false
+                            }
+                        },
+                        isRefreshing = isRefreshing,
+                        userDoseLog = userDoseLog,
+                        onPokeClick = {
+                            if (memberId != null) {
+                                viewModel.postFcmPushNotification(memberId)
+                            }
                         }
-                    },
-                    isRefreshing = isRefreshing,
-                    userDoseLog = userDoseLog
-                )
+                    )
+                }
             }
         }
-        if(relationInfoList.isNotEmpty()) {
+        if (isButtonVisible) {
             ScheduleAddButton(
                 Modifier
                     .align(Alignment.BottomEnd)
                     .padding(bottom = 12.dp, end = 16.dp),
-            ) {
-                navController.navigate("scheduleAddScreen")
-            }
+                buttonState = buttonState,
+                onClick = {
+                    Log.e("scheduleAddScreen", "On Navigate With $memberId")
+                    navController.navigate("scheduleAddScreen/${memberId}")
+                }
+            )
         }
     }
-    LaunchedEffect(userDetails) {
-        userDetails?.let {
-            mainViewModel.getUserDoseLog(it.memberId)
+    LaunchedEffect(memberId) {
+        memberId?.let {
+            mainViewModel.getUserDoseLog(it)
         }
     }
     LaunchedEffect(true) {
